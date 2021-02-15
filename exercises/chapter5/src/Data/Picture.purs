4a15c42f4fce0d7f4713afcae02fabf940f58bac
Andy White <andywhite441@gmail.com>
2021-02-14 21:34:45 -0700

    chapter 5

--- exercises/chapter5/src/Data/Picture.purs
+++ exercises/chapter5/src/Data/Picture.purs
@@ -5,130 +5,151 @@ import Data.Foldable (foldl)
 import Global as Global
 import Math as Math
 
-type Point =
-  { x :: Number
-  , y :: Number
-  }
+type Point
+  = { x :: Number
+    , y :: Number
+    }
 
 showPoint :: Point -> String
-showPoint { x, y } =
-  "(" <> show x <> ", " <> show y <> ")"
+showPoint { x, y } = "(" <> show x <> ", " <> show y <> ")"
 
 data Shape
   = Circle Point Number
   | Rectangle Point Number Number
   | Line Point Point
   | Text Point String
+  | Clipped Point Number Number Picture
 
 showShape :: Shape -> String
-showShape (Circle c r) =
-  "Circle [center: " <> showPoint c <> ", radius: " <> show r <> "]"
-showShape (Rectangle c w h) =
-  "Rectangle [center: " <> showPoint c <> ", width: " <> show w <> ", height: " <> show h <> "]"
-showShape (Line start end) =
-  "Line [start: " <> showPoint start <> ", end: " <> showPoint end <> "]"
-showShape (Text loc text) =
-  "Text [location: " <> showPoint loc <> ", text: " <> show text <> "]"
+showShape (Circle c r) = "Circle [center: " <> showPoint c <> ", radius: " <> show r <> "]"
+
+showShape (Rectangle c w h) = "Rectangle [center: " <> showPoint c <> ", width: " <> show w <> ", height: " <> show h <> "]"
+
+showShape (Line start end) = "Line [start: " <> showPoint start <> ", end: " <> showPoint end <> "]"
+
+showShape (Text loc text) = "Text [location: " <> showPoint loc <> ", text: " <> show text <> "]"
+
+showShape (Clipped _ _ _ _) = "Clipped"
 
 exampleLine :: Shape
 exampleLine = Line p1 p2
   where
-    p1 :: Point
-    p1 = { x: 0.0, y: 0.0 }
+  p1 :: Point
+  p1 = { x: 0.0, y: 0.0 }
 
-    p2 :: Point
-    p2 = { x: 100.0, y: 50.0 }
+  p2 :: Point
+  p2 = { x: 100.0, y: 50.0 }
 
 origin :: Point
 origin = { x, y }
   where
-    x = 0.0
-    y = 0.0
+  x = 0.0
+
+  y = 0.0
+
 -- Would generally write it like this instead:
 -- origin = { x: 0.0, y: 0.0 }
-
 getCenter :: Shape -> Point
 getCenter (Circle c r) = c
+
 getCenter (Rectangle c w h) = c
-getCenter (Line s e) = (s + e) * {x: 0.5, y: 0.5}
+
+getCenter (Line s e) = (s + e) * { x: 0.5, y: 0.5 }
+
 getCenter (Text loc text) = loc
 
-type Picture = Array Shape
+getCenter (Clipped c w h _) = c
+
+type Picture
+  = Array Shape
 
 showPicture :: Picture -> Array String
 showPicture = map showShape
 
-type Bounds =
-  { top    :: Number
-  , left   :: Number
-  , bottom :: Number
-  , right  :: Number
-  }
+type Bounds
+  = { top :: Number
+    , left :: Number
+    , bottom :: Number
+    , right :: Number
+    }
 
 showBounds :: Bounds -> String
 showBounds b =
-  "Bounds [top: " <> show b.top <>
-  ", left: "      <> show b.left <>
-  ", bottom: "    <> show b.bottom <>
-  ", right: "     <> show b.right <>
-  "]"
+  "Bounds [top: " <> show b.top
+    <> ", left: "
+    <> show b.left
+    <> ", bottom: "
+    <> show b.bottom
+    <> ", right: "
+    <> show b.right
+    <> "]"
 
 shapeBounds :: Shape -> Bounds
 shapeBounds (Circle { x, y } r) =
-  { top:    y - r
-  , left:   x - r
+  { top: y - r
+  , left: x - r
   , bottom: y + r
-  , right:  x + r
+  , right: x + r
   }
+
 shapeBounds (Rectangle { x, y } w h) =
-  { top:    y - h / 2.0
-  , left:   x - w / 2.0
+  { top: y - h / 2.0
+  , left: x - w / 2.0
   , bottom: y + h / 2.0
-  , right:  x + w / 2.0
+  , right: x + w / 2.0
   }
+
 shapeBounds (Line p1 p2) =
-  { top:    Math.min p1.y p2.y
-  , left:   Math.min p1.x p2.x
+  { top: Math.min p1.y p2.y
+  , left: Math.min p1.x p2.x
   , bottom: Math.max p1.y p2.y
-  , right:  Math.max p1.x p2.x
+  , right: Math.max p1.x p2.x
   }
+
 shapeBounds (Text { x, y } _) =
-  { top:    y
-  , left:   x
+  { top: y
+  , left: x
   , bottom: y
-  , right:  x
+  , right: x
+  }
+
+shapeBounds (Clipped { x, y } w h _) =
+  { top: y - h / 2.0
+  , left: x - w / 2.0
+  , bottom: y + h / 2.0
+  , right: x + w / 2.0
   }
 
 union :: Bounds -> Bounds -> Bounds
 union b1 b2 =
-  { top:    Math.min b1.top    b2.top
-  , left:   Math.min b1.left   b2.left
+  { top: Math.min b1.top b2.top
+  , left: Math.min b1.left b2.left
   , bottom: Math.max b1.bottom b2.bottom
-  , right:  Math.max b1.right  b2.right
+  , right: Math.max b1.right b2.right
   }
 
 intersect :: Bounds -> Bounds -> Bounds
 intersect b1 b2 =
-  { top:    Math.max b1.top    b2.top
-  , left:   Math.max b1.left   b2.left
+  { top: Math.max b1.top b2.top
+  , left: Math.max b1.left b2.left
   , bottom: Math.min b1.bottom b2.bottom
-  , right:  Math.min b1.right  b2.right
+  , right: Math.min b1.right b2.right
   }
 
 emptyBounds :: Bounds
 emptyBounds =
-  { top:     Global.infinity
-  , left:    Global.infinity
+  { top: Global.infinity
+  , left: Global.infinity
   , bottom: -Global.infinity
-  , right:  -Global.infinity
+  , right: -Global.infinity
   }
 
 infiniteBounds :: Bounds
 infiniteBounds =
-  { top:    -Global.infinity
-  , left:   -Global.infinity
-  , bottom:  Global.infinity
-  , right:   Global.infinity
+  { top: -Global.infinity
+  , left: -Global.infinity
+  , bottom: Global.infinity
+  , right: Global.infinity
   }
 
 bounds :: Picture -> Bounds
--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -1,1 +1,91 @@
 module Test.MySolutions where
+
+import Prelude
+import ChapterExamples (Amp(..), Volt(..))
+import Data.Maybe (Maybe(..))
+import Data.Person (Person)
+import Data.Picture (Point, Shape(..), showShape)
+import Math as Math
+
+factorial :: Int -> Int
+factorial 0 = 1
+
+factorial 1 = 1
+
+factorial n = n * factorial (n - 1)
+
+binomial :: Int -> Int -> Int
+binomial _ 0 = 1
+
+binomial 0 _ = 0
+
+binomial n k
+  | n < k = 0
+  | otherwise = (factorial n) / ((factorial k) * factorial (n - k))
+
+pascal :: Int -> Int -> Int
+pascal _ 0 = 1
+
+pascal 0 _ = 0
+
+pascal n k = pascal (n - 1) k + pascal (n - 1) (k - 1)
+
+sameCity :: Person -> Person -> Boolean
+sameCity { address: { city: city1 } } { address: { city: city2 } } = city1 == city2
+
+fromSingleton :: ∀ a. a -> Array a -> a
+fromSingleton _ [ a ] = a
+
+fromSingleton a _ = a
+
+circleAtOrigin :: Shape
+circleAtOrigin = Circle { x: 0.0, y: 0.0 } 10.0
+
+doubleScaleAndCenter :: Shape -> Shape
+doubleScaleAndCenter (Circle _ r) = Circle { x: 0.0, y: 0.0 } $ r * 2.0
+
+doubleScaleAndCenter (Rectangle _ w h) = Rectangle { x: 0.0, y: 0.0 } (w * 2.0) (h * 2.0)
+
+doubleScaleAndCenter (Line p1 p2) =
+  let
+    oldW = Math.abs (p1.x - p2.x)
+
+    oldH = Math.abs (p1.y - p2.y)
+
+    w = 2.0 * oldW
+
+    h = 2.0 * oldH
+  in
+    Line { x: -oldW, y: -oldH } { x: -oldW + w, y: -oldH + h }
+
+doubleScaleAndCenter (Text p text) = Text { x: 0.0, y: 0.0 } text
+
+doubleScaleAndCenter (Clipped _ w h p) = Clipped { x: 0.0, y: 0.0 } (w * 2.0) (h * 2.0) p
+
+shapeText :: Shape -> Maybe String
+shapeText (Text _ text) = Just text
+
+shapeText _ = Nothing
+
+newtype Watt
+  = Watt Number
+
+calculateWattage :: Amp -> Volt -> Watt
+calculateWattage (Amp i) (Volt v) = Watt (v * i)
+
+type Picture
+  = Array Shape
+
+showPicture :: Picture -> Array String
+showPicture = map showShape
+
+area :: Shape -> Number
+area (Circle _ r) = Math.pi * r * r
+
+area (Rectangle _ w h) = w * h
+
+area (Line _ _) = 0.0
+
+area (Text _ _) = 0.0
+
+area (Clipped _ w h _) = w * h
